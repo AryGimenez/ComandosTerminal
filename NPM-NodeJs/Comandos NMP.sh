bbec9860ffdb9e5053896a1d2cc3669da8694677
Ary Gimenez <arygimenez@localhost.localdomain>
2021-03-13 08:05:26 -0500

    Agregue comandos a npm. Creo haber estado estudiando cómo agregar un Proyecto a node.Js

--- NPM-NodeJs/Comandos NMP.sh
+++ NPM-NodeJs/Comandos NMP.sh
@@ -4,8 +4,17 @@ npm -v
 -- Muestra la vercion acutal de Node.Js
 node -v
 
+# -- Ejectua el Node.js
+npm start
+
+# -- inica un proyecto generando un
+#    archivo packate.json
+npm init
+
+npm init -f # <?>
+
 -- actualiza npm
 npm install npm@lastest -g
 
-# -- Desitnala Paquetres 
+# -- Desitnala Paquetres
 npm unistall 'paquete'
