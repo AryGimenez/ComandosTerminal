6a05ba8e3bb88cb2c21b67c7860e8e233afbce57
AryGimenez <argi.prog@gmail.com>
2021-07-31 03:07:57 -0400

    agregar comandos para trabajar con ssh y detectar puertos abiertos en un host local o remto

--- linuxComand.sh
+++ linuxComand.sh
@@ -6,10 +6,28 @@ date --set "2019-11-31 15:20" # modifica fecha actualice
 # -- Apagar en 30 m
 sudo shutdown -h +30
 
+# -- <F> Para contectarme por ssh
+ssh ary@<IP/HOST>
+
+
+# -- Instala ssh
+sudo apt install ssh <Ubuntu>
+
+# -- Muestra estado de ssh
+systemctl status ssh.service
+
 sudo shutdown now # apaga en el movimiento
 
 sudo sutdown -r  # <F> Reinicia el sistema operativo
 
+# -- Escanea los puertos abiertos de un Host o IP Los escanea por TCP o UDB
+sudo nmap -s<U/T> -O Ip/<IP/Host>
+
+# -- <?>
+sudo lsof -i -P -n # <?> Se que escanea puertos pero no me quedo claro porque entrega mucha informacion https://ubunlog.com/como-comprobar-los-puertos-en-uso-en-linux/
+
+# -- <?>
+netstat -putona | grep numero-de-puerto # <?> Se que escanea puertos No lo mire lo saque https://ubunlog.com/como-comprobar-los-puertos-en-uso-en-linux/
 
 #-- Crear un USB de instalacion linux
 # IDENTIFICA EL USB
